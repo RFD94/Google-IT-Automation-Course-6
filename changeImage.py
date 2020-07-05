0b08a3e2c895e8dcdb30a1852bf02e57fe5aa5c2
RFD94 <34864864+RFD94@users.noreply.github.com>
2020-07-05 12:58:08 +0200

    Add files via upload

--- changeImage.py
+++ changeImage.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
- 
-import os, glob
+
+import os, sys
+#import the module first "pip3 install pillow"
 from PIL import Image
 
-for file in glob.glob("ic_*"):
-       im = Image.open(file).convert('RGB')
-       im.rotate(270).resize((128,128)).save("/opt/icons/" + file, "JPEG")
+user = os.getenv('USER') # To get the username from environment variable
+image_directory = '/home/{}/supplier-data/images/'.format(user)
+for image_name in os.listdir(image_directory):
+    if not image_name.startswith('.') and 'tiff' in image_name:
+        image_path = image_directory + image_name
+        path = os.path.splitext(image_path)[0]
+        im = Image.open(image_path)
+        new_path = '{}.jpeg'.format(path)
+        im.convert('RGB').resize((600, 400)).save(new_path, "JPEG")
--- run.py
+++ run.py
@@ -1,16 +1,34 @@
-#!/user/bin/env python3
-import os
-import requests
+!/usr/bin/env python3
 
-lists = []
-filepath = '/data/feedback'
-external_ip = "34.69.76.32"
+import os, requests, json
 
-for file in folder:
-    with open(filepath + file, 'r') as f:
-        lists.append({"title": f.readline().rstrip("\n"),"name": f.readline().rstrip("\n"),"date": f.readline().rstrip("\n"),"feedback": f.readline().rstrip("\n")})
-        for item in lists:
-            resp = requests.post("http://" + external_ip + "/feedback/", json=item)
-            if resp.status_code != 201:
-                raise Exception('POST error status={}'.format(resp.status_code))
-            print('Created feedback ID {}'.format(resp.json()["id"]))
+def catalog_data(url,description_dir):
+    """This function will return a list of dictionaries with all the details like name, weight, description, image_n$
+    It will change the weight to integer format.
+    """
+    fruit={}
+    for item in os.listdir(description_dir):
+      fruit.clear()
+      filename=os.path.join(description_dir,item)
+      with open(filename) as f:
+        line=f.readlines()
+        description=""
+        for i in range(2,len(line)):
+          description=description+line[i].strip('\n').replace(u'\xa0',u'')
+        fruit["description"]=description
+        fruit["weight"]=int(line[1].strip('\n').strip('lbs'))
+        fruit["name"]=line[0].strip('\n')
+        fruit["image_name"]=(item.strip('.txt'))+'.jpeg'
+        print(fruit)
+        if url!="":
+          response=requests.post(url, json=fruit)
+          print(response.request.url)
+          print(response.status_code)
+    return 0
+
+if __name__=='__main__':
+    url = 'http://localhost/fruits/'
+    user = os.getenv('USER')
+    description_directory = '/home/{}/supplier-data/descriptions/'.format(user)
+    catalog_data(url,description_directory)
+
